3259fbf5166be398aac90c70f6460aa3c11a34c0
daxp472 <daxpatel.cg@gmail.com>
2025-06-20 11:05:29 +0530

    client dashboard page added

---
+++ Frontend-New/src/pages/client/ClientDashboard.tsx
@@ -0,0 +1,27 @@
+import { Routes, Route } from 'react-router-dom'
+import { ClientLayout } from '@/components/layouts/ClientLayout'
+import { ClientOverview } from './ClientOverview'
+import { ClientProjects } from './ClientProjects'
+import { ClientProjectDetail } from './ClientProjectDetail'
+import { ClientFiles } from './ClientFiles'
+import { ClientNotifications } from './ClientNotifications'
+import { ClientRevisions } from './ClientRevisions'
+import { ClientSupport } from './ClientSupport'
+import { ClientSettings } from './ClientSettings'
+
+export function ClientDashboard() {
+  return (
+    <ClientLayout>
+      <Routes>
+        <Route path="/" element={<ClientOverview />} />
+        <Route path="/projects" element={<ClientProjects />} />
+        <Route path="/projects/:id" element={<ClientProjectDetail />} />
+        <Route path="/files" element={<ClientFiles />} />
+        <Route path="/notifications" element={<ClientNotifications />} />
+        <Route path="/revisions" element={<ClientRevisions />} />
+        <Route path="/support" element={<ClientSupport />} />
+        <Route path="/settings" element={<ClientSettings />} />
+      </Routes>
+    </ClientLayout>
+  )
+}
