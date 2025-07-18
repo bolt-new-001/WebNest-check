84f0f9fd3418b958eb33b8b03bb7c97958a2bb18
daxp472 <daxpatel.cg@gmail.com>
2025-07-18 11:32:51 +0530

    Devloper Dashboard file modified

--- Frontend-New/src/pages/developer/DeveloperDashboard.tsx
+++ Frontend-New/src/pages/developer/DeveloperDashboard.tsx
@@ -2,7 +2,13 @@ import { Routes, Route } from 'react-router-dom'
 import { DeveloperLayout } from '@/components/layouts/DeveloperLayout'
 import { DeveloperOverview } from './DeveloperOverview'
 import { DeveloperProjects } from './DeveloperProjects'
-// More pages will be added here
+import { DeveloperProjectDetail } from './DeveloperProjectDetail'
+import { DeveloperPortfolio } from './DeveloperPortfolio'
+import { DeveloperEarnings } from './DeveloperEarnings'
+import { DeveloperTimeTracking } from './DeveloperTimeTracking'
+import { DeveloperMessages } from './DeveloperMessages'
+import { DeveloperAvailability } from './DeveloperAvailability'
+import { DeveloperSettings } from './DeveloperSettings'
 
 export function DeveloperDashboard() {
   return (
@@ -10,7 +16,13 @@ export function DeveloperDashboard() {
       <Routes>
         <Route path="/" element={<DeveloperOverview />} />
         <Route path="/projects" element={<DeveloperProjects />} />
-        {/* Add more routes as needed */}
+        <Route path="/projects/:id" element={<DeveloperProjectDetail />} />
+        <Route path="/portfolio" element={<DeveloperPortfolio />} />
+        <Route path="/earnings" element={<DeveloperEarnings />} />
+        <Route path="/time-tracking" element={<DeveloperTimeTracking />} />
+        <Route path="/messages" element={<DeveloperMessages />} />
+        <Route path="/availability" element={<DeveloperAvailability />} />
+        <Route path="/settings" element={<DeveloperSettings />} />
       </Routes>
     </DeveloperLayout>
   )
